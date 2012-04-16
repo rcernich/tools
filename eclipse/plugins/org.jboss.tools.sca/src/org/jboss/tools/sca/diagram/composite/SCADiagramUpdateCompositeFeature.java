3697e369bef00b7be7e18ba911cfa63989586b10
bfitzpat <bfitzpat@redhat.com>
2012-04-16 11:38:09 -0600

    Fixing the name out of date issue

--- eclipse/plugins/org.jboss.tools.sca/src/org/jboss/tools/sca/diagram/composite/SCADiagramUpdateCompositeFeature.java
+++ eclipse/plugins/org.jboss.tools.sca/src/org/jboss/tools/sca/diagram/composite/SCADiagramUpdateCompositeFeature.java
@@ -5,6 +5,7 @@ import org.eclipse.graphiti.features.IReason;
 import org.eclipse.graphiti.features.context.IUpdateContext;
 import org.eclipse.graphiti.features.impl.AbstractUpdateFeature;
 import org.eclipse.graphiti.features.impl.Reason;
+import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
 import org.eclipse.graphiti.mm.algorithms.Text;
 import org.eclipse.graphiti.mm.pictograms.ContainerShape;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
@@ -25,6 +26,19 @@ public class SCADiagramUpdateCompositeFeature extends AbstractUpdateFeature {
 		return (bo instanceof Composite);
 	}
 
+	private Text findText (GraphicsAlgorithm root) {
+		for (GraphicsAlgorithm ga : root.getGraphicsAlgorithmChildren()) {
+			if (ga instanceof Text) {
+				Text text = (Text) ga;
+				return text;
+			}
+			if (ga.getGraphicsAlgorithmChildren().size() > 0) {
+				return findText(ga);
+			}
+		}
+		return null;
+	}
+	
 	@Override
 	public IReason updateNeeded(IUpdateContext context) {
 		// retrieve name from pictogram model
@@ -32,11 +46,9 @@ public class SCADiagramUpdateCompositeFeature extends AbstractUpdateFeature {
 		PictogramElement pictogramElement = context.getPictogramElement();
 		if (pictogramElement instanceof ContainerShape) {
 			ContainerShape cs = (ContainerShape) pictogramElement;
-			for (Shape shape : cs.getChildren()) {
-				if (shape.getGraphicsAlgorithm() instanceof Text) {
-					Text text = (Text) shape.getGraphicsAlgorithm();
-					pictogramName = text.getValue();
-				}
+			Text foundText = findText(cs.getGraphicsAlgorithm());
+			if (foundText != null) {
+				pictogramName = foundText.getValue();
 			}
 		}
 
@@ -50,9 +62,9 @@ public class SCADiagramUpdateCompositeFeature extends AbstractUpdateFeature {
 			// update needed, if names are different
 			boolean updateNameNeeded =
 					((pictogramName == null && businessName != null) || 
-							(pictogramName != null && !pictogramName.equals(businessName)));
+							(pictogramName != null && !pictogramName.contentEquals(businessName)));
 			if (updateNameNeeded) {
-				return Reason.createTrueReason("Name is out of date");
+				return Reason.createTrueReason("Composite name is out of date");
 			} else {
 				return Reason.createFalseReason();
 			}
