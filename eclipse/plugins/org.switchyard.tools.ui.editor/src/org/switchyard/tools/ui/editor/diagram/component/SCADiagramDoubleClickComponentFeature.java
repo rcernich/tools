90b50bb293ecb7b76f38d4560163625b60120ab1
bfitzpat <bfitzpat@redhat.com>
2012-05-07 13:43:20 -0600

    Adding new files for double-click support

---
+++ eclipse/plugins/org.switchyard.tools.ui.editor/src/org/switchyard/tools/ui/editor/diagram/component/SCADiagramDoubleClickComponentFeature.java
@@ -0,0 +1,36 @@
+package org.switchyard.tools.ui.editor.diagram.component;
+
+import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.context.ICustomContext;
+import org.eclipse.graphiti.features.custom.AbstractCustomFeature;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * @author bfitzpat
+ *
+ */
+public class SCADiagramDoubleClickComponentFeature extends AbstractCustomFeature {
+
+    /**
+     * @param fp Feature provider
+     */
+    public SCADiagramDoubleClickComponentFeature(IFeatureProvider fp) {
+        super(fp);
+    }
+
+    @Override
+    public void execute(ICustomContext context) {
+        MessageDialog.openInformation(PlatformUI.getWorkbench()
+                .getActiveWorkbenchWindow().getShell(), "Information",
+             "Double-click on a Component has not been implemented yet.");
+
+
+    }
+
+    @Override
+    public boolean canExecute(ICustomContext context) {
+        return true;
+    }
+
+}
